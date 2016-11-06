2f7017e51781247b802214bd40613731727c0fb6
Daniel <yuchzhou@amazon.com>
2016-11-05 23:36:21 -0700

    working version

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
 fancy_echo() {
-  printf "\n%b\n" "$1"
+  local fmt="$1"; shift
+  printf "\n$fmt\n" "$@"
 }
 
 install_if_needed() {
@@ -13,16 +16,16 @@ install_if_needed() {
 
 append_to_zshrc() {
   local text="$1" zshrc
-  local skip_new_line="$2"
+  local skip_new_line="${2:-0}"
 
-  if [[ -w "$HOME/.zshrc.local" ]]; then
+  if [ -w "$HOME/.zshrc.local" ]; then
     zshrc="$HOME/.zshrc.local"
   else
     zshrc="$HOME/.zshrc"
   fi
 
   if ! grep -Fqs "$text" "$zshrc"; then
-    if (( skip_new_line )); then
+    if [ "$skip_new_line" -eq 1 ]; then
       printf "%s\n" "$text" >> "$zshrc"
     else
       printf "\n%s\n" "$text" >> "$zshrc"
@@ -30,7 +33,18 @@ append_to_zshrc() {
   fi
 }
 
-#!/usr/bin/env bash
+find_latest_ruby() {
+  rbenv install -l | grep -v - | tail -1 | sed -e 's/^ *//'
+}
+
+gem_install_or_update() {
+  if gem list "$1" --installed > /dev/null; then
+    gem update "$@"
+  else
+    gem install "$@"
+    rbenv rehash
+  fi
+}
 
 trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
 set -e
@@ -43,13 +57,6 @@ if [ ! -f "$HOME/.zshrc" ]; then
   touch "$HOME/.zshrc"
 fi
 
-append_to_zshrc 'export PATH="$HOME/.bin:$PATH"'
-
-if ! grep -qiE 'wheezy|jessie|precise|trusty' /etc/os-release; then
-  fancy_echo "Sorry! we don't currently support that distro."
-  exit 1
-fi
-
 fancy_echo "Updating system packages ..."
   if command -v aptitude >/dev/null; then
     fancy_echo "Using aptitude ..."
@@ -58,153 +65,87 @@ fancy_echo "Updating system packages ..."
     sudo apt-get install -y aptitude
   fi
 
-  sudo aptitude update
+sudo aptitude update
 
 fancy_echo "Installing git, for source control management ..."
-  install_if_needed git
-
-fancy_echo "Installing base ruby build dependencies ..."
-  sudo aptitude build-dep -y ruby1.9.3
+install_if_needed git
 
 fancy_echo "Installing libraries for common gem dependencies ..."
-  sudo aptitude install -y libxslt1-dev libcurl4-openssl-dev libksba8 libksba-dev libqtwebkit-dev libreadline-dev
-
-fancy_echo "Installing sqlite3, for prototyping database-backed rails apps"
-  install_if_needed libsqlite3-dev
-  install_if_needed sqlite3
-
-fancy_echo "Installing Postgres, a good open source relational database ..."
-  install_if_needed postgresql
-  install_if_needed postgresql-server-dev-all
-
-fancy_echo "Installing Redis, a good key-value database ..."
-  install_if_needed redis-server
+sudo aptitude install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 
 fancy_echo "Installing ctags, to index files for vim tab completion of methods, classes, variables ..."
-  install_if_needed exuberant-ctags
+install_if_needed exuberant-ctags
 
 fancy_echo "Installing vim ..."
-  install_if_needed vim-gtk
+install_if_needed vim
+sh <(curl https://j.mp/spf13-vim3 -L)
 
 fancy_echo "Installing tmux, to save project state and switch between projects ..."
-  install_if_needed tmux
-
-fancy_echo "Installing ImageMagick, to crop and resize images ..."
-  install_if_needed imagemagick
+install_if_needed tmux
 
 fancy_echo "Installing watch, to execute a program periodically and show the output ..."
-  install_if_needed watch
+install_if_needed watch
 
 fancy_echo "Installing curl ..."
-  install_if_needed curl
+install_if_needed curl
 
 fancy_echo "Installing zsh ..."
-  install_if_needed zsh
+install_if_needed zsh
+
+fancy_echo "Installing The Silver Searcher (better than ack or grep) to search the contents of files ..."
+install_if_needed silversearcher-ag
+
+fancy_echo "Installing default JDK ..."
+install_if_needed default-jdk
+
+fancy_echo "Installing maven ..."
+install_if_needed mvn
 
 fancy_echo "Installing node, to render the rails asset pipeline ..."
-  install_if_needed nodejs
+install_if_needed nodejs
+
+fancy_echo "Installing pip ..."
+install_if_needed python-pip
+
+fancy_echo "Installing oh-my-zsh ..."
+rm -rf "$HOME/.oh-my-zsh"
+git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
 
 fancy_echo "Changing your shell to zsh ..."
-  chsh -s $(which zsh)
+chsh -s $(which zsh)
 
-silver_searcher_from_source() {
-  git clone git://github.com/ggreer/the_silver_searcher.git /tmp/the_silver_searcher
-  sudo aptitude install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
-  sh /tmp/the_silver_searcher/build.sh
-  cd /tmp/the_silver_searcher
-  sh build.sh
-  sudo make install
-  cd
-  rm -rf /tmp/the_silver_searcher
-}
+fancy_echo "Configuring Python ..."
+sudo pip install --upgrade pip
+pip install --user virtualenv
+pip install --user virtualenvwrapper
+append_to_zshrc 'export PATH="$HOME/.local/bin:$PATH"'
+append_to_zshrc 'export WORKON_HOME="$HOME/.virtualenvs"'
+append_to_zshrc 'source "$HOME/.local/bin/virtualenvwrapper.sh"'
 
-if ! command -v ag >/dev/null; then
-  fancy_echo "Installing The Silver Searcher (better than ack or grep) to search the contents of files ..."
+fancy_echo "Configuring Ruby ..."
+rm -rf "$HOME/.rbenv"
+git clone https://github.com/rbenv/rbenv.git "$HOME/.rbenv"
 
-  if aptitude show silversearcher-ag &>/dev/null; then
-    install_if_needed silversearcher-ag
-  else
-    silver_searcher_from_source
-  fi
+append_to_zshrc 'export PATH="$HOME/.rbenv/bin:$PATH"'
+append_to_zshrc 'eval "$(rbenv init - zsh)"' 1
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - zsh)"
+
+if [[ ! -d "$HOME/.rbenv/plugins/ruby-build" ]]; then
+  fancy_echo "Installing ruby-build, to install Rubies ..."
+  git clone https://github.com/rbenv/ruby-build.git "$HOME/.rbenv/plugins/ruby-build"
 fi
 
-chruby_from_source() {
-  wget -O /tmp/chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-  cd /tmp/
-  tar -xzvf chruby-0.3.9.tar.gz
-  cd /tmp/chruby-0.3.9/
-  sudo make install
-  cd
-  rm -rf /tmp/chruby-0.3.9/
+ruby_version="$(find_latest_ruby)"
+if ! rbenv versions | grep -Fq "$ruby_version"; then
+  rbenv install -s "$ruby_version"
+  rbenv rehash
+fi
+  
+rbenv global "$ruby_version"
+rbenv shell "$ruby_version"
+gem update --system
 
-  append_to_zshrc 'source /usr/local/share/chruby/chruby.sh'
-  append_to_zshrc 'source /usr/local/share/chruby/auto.sh'
-}
-
-ruby_install_from_source() {
-  wget -O /tmp/ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
-  cd /tmp/
-  tar -xzvf ruby-install-0.5.0.tar.gz
-  cd /tmp/ruby-install-0.5.0/
-  sudo make install
-  cd
-  rm -rf /tmp/ruby-install-0.5.0/
-}
-
-chruby_from_source
-ruby_version="$(curl -sSL http://ruby.thoughtbot.com/latest)"
-
-fancy_echo "Installing ruby-install for super easy installation of rubies..."
-  ruby_install_from_source
-
-fancy_echo "Installing Ruby $ruby_version ..."
-  ruby-install ruby "$ruby_version"
-
-fancy_echo "Loading chruby and changing to Ruby $ruby_version ..."
-  source ~/.zshrc
-  chruby $ruby_version
-
-fancy_echo "Setting default Ruby to $ruby_version ..."
-  append_to_zshrc "chruby ruby-$ruby_version"
-
-fancy_echo "Updating to latest Rubygems version ..."
-  gem update --system
-
-fancy_echo "Installing Bundler to install project-specific Ruby gems ..."
-  gem install bundler --no-document --pre
-
-fancy_echo "Configuring Bundler for faster, parallel gem installation ..."
-  number_of_cores=$(nproc)
-  bundle config --global jobs $((number_of_cores - 1))
-
-fancy_echo "Installing Suspenders, thoughtbot's Rails template ..."
-  gem install suspenders --no-document
-
-fancy_echo "Installing Parity, shell commands for development, staging, and production parity ..."
-  gem install parity --no-document
-
-fancy_echo "Installing Heroku CLI client ..."
-  curl -s https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
-fancy_echo "Installing the heroku-config plugin to pull config variables locally to be used as ENV variables ..."
-  heroku plugins:install git://github.com/ddollar/heroku-config.git
-
-fancy_echo "Installing GitHub CLI client ..."
-  version="$(curl https://github.com/jingweno/gh/releases/latest -s | cut -d'v' -f2 | cut -d'"' -f1)"
-
-  if uname -m | grep -Fq 'x86_64'; then
-    arch='amd64'
-  else
-    arch='i386'
-  fi
-
-  cd /tmp
-  url="https://github.com/jingweno/gh/releases/download/v${version}/gh_${version}_${arch}.deb"
-  curl "$url" -sLo gh.deb
-  sudo dpkg -i gh.deb
-  cd -
-
-fancy_echo "Installing rcm, to manage your dotfiles ..."
-  wget -O /tmp/rcm_1.2.3-1_all.deb https://thoughtbot.github.io/rcm/debs/rcm_1.2.3-1_all.deb
-  sudo dpkg -i /tmp/rcm_1.2.3-1_all.deb
-  rm -f /tmp/rcm_1.2.3-1_all.deb
+gem_install_or_update 'bundler'
